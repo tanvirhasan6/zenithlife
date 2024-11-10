7626d6000dbb65762504da4aa8ca492e581e81b5
tanvirhasan6 <tanvir.hasan6@gmail.com>
2024-11-10 17:55:46 +0600

    working on dropdown big menu

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -16,15 +16,13 @@ const Navbar = () => {
 
     return (
 
-        <div className='w-full'>
-            <div className="sticky top-0 z-10 navbar bg-white shadow-sm">
+        <div className='w-full relative'>
+            <div className="fixed top-0 z-10 navbar bg-white shadow-sm">
                 <div className='navbar-start'>
-                    <div className="dropdown">
-                        <div>
-                            {
-                                dropdown ? <RiCloseLargeFill onClick={handleDropDown} /> : <CgMenuLeft onClick={handleDropDown} />
-                            }
-                        </div>
+                    <div>
+                        {
+                            dropdown ? <RiCloseLargeFill onClick={handleDropDown} /> : <CgMenuLeft onClick={handleDropDown} />
+                        }
                     </div>
                 </div>
                 <div className="navbar-center">
@@ -32,7 +30,7 @@ const Navbar = () => {
                         <img className='w-[80px] md:w-[120px]' src={logo} alt="logo" />
                     </NavLink>
                 </div>
-                <div className='navbar-end'>
+                <div className='navbar-end gap-2'>
                     <FaLanguage />
                     <select className="text-xs pl-1">
                         <option>English</option>
@@ -41,25 +39,28 @@ const Navbar = () => {
                 </div>
             </div >
 
-            <div className={`w-full h-[450px] ${dropdown ? 'flex' : 'hidden'}`}>
+            <div className={`w-full h-screen ${dropdown ? 'flex' : 'hidden'} grid-cols-2 md:grid-cols-3 justify-around items-start bg-green-100 z-10 fixed top-16 left-0 pt-4 shadow-sm`}>
 
-                <nav>
+                <div className='flex flex-col justify-start items-start gap-4'>
+                    <h5 className='font-bold text-lg'>About Us</h5>
                     <a className="link link-hover">Branding</a>
                     <a className="link link-hover">Design</a>
                     <a className="link link-hover">Marketing</a>
                     <a className="link link-hover">Advertisement</a>
-                </nav>
-                <nav>
+                </div>
+                <div className='flex flex-col justify-start items-start gap-4'>
+                    <h5 className='font-bold text-lg'>Service</h5>
                     <a className="link link-hover">About us</a>
                     <a className="link link-hover">Contact</a>
                     <a className="link link-hover">Jobs</a>
                     <a className="link link-hover">Press kit</a>
-                </nav>
-                <nav>
+                </div>
+                <div className='flex flex-col justify-start items-start gap-4'>
+                    <h5 className='font-bold text-lg'>Support</h5>
                     <a className="link link-hover">Terms of use</a>
                     <a className="link link-hover">Privacy policy</a>
                     <a className="link link-hover">Cookie policy</a>
-                </nav>
+                </div>
 
             </div>
 
